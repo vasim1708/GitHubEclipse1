bbb94cde6885cc17344f7b1336f446427f4d3694
vasim4518 <vasimtamboli7261@gmail.com>
2022-11-14 14:08:23 +0530

    3 commit

--- GitHubEclipse1/src/com/lti/demo/TestProducts.java
+++ GitHubEclipse1/src/com/lti/demo/TestProducts.java
@@ -3,8 +3,8 @@ package com.lti.demo;
 
 public class TestProducts {
 
-    public static void main(String[] args) {  
-      System.out.println("testing product" );  
+public static void main(String[] args) {
+      System.out.println("testing product");
     }
 }
 
